eaa9f0a02100e94f4c1b9b52b002bdf37c09a551
Evans <snaveqiptoo@gmail.com>
2023-06-18 10:45:28 -0700

    Printf Fucntion

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#inlcude <stdio.h>
+int main(void)
+/**
+ *main - this is the main function
+ *Return: always 0
+ */
+{ printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
