86bae64c18e8476ad8917a5c2cb61aee1056b110
Evans <snaveqiptoo@gmail.com>
2023-06-25 19:11:43 -0700

    Print alphabets in small

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- *print_alphabet: This progrma prints letters of the alphabet in small caps.
+ *print_alphabet-This progrma prints letters of the alphabet in small caps.
  */
+
 void print_alphabet(void){
 	char letter;
 	for (letter='a';letter<='z';letter ++)
 		_putchar(letter);
-
 	_putchar('\n');
-
 }
-
